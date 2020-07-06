e5714a4b9f5faeaa6d0802b1140520726aeaf590
Maksym Kravchuk <kravchukmaxym@gmail.com>
2020-07-06 12:17:33 +0300

    'AOS' (Animate on scroll library) added

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,21 @@
 let $ = require('jquery')
+let AOS = require('aos')
 let slick = require('./slick.js')
 
 $(function() {
+  $('h1, h2, h3, h4, h5, h6, img').attr("data-aos", "fade-up")
+
+  AOS.init({
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 600, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom',
+  });
+  setTimeout(AOS.refresh, 500)
+
   const $body = $('body')
   const $nav = $('nav')
 
@@ -194,7 +208,7 @@ $(function() {
   }
 })
 
-$(window).load(function() {
+$(window).on('load', function() {
   function goToByScroll(id) {
     $("html, body").animate({
       scrollTop: $("#" + id).offset().top - 100
